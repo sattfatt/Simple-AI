dd074793976b4de624c7adbcdae6de70c9a843a9
sattfatt <sattfatt@gmail.com>
2017-08-06 14:45:44 -0700

    Added projectile system with mouse aiming.

--- pygame1/test.py
+++ pygame1/test.py
@@ -2,12 +2,17 @@ import pygame, sys
 from pygame.locals import *
 from pygame import gfxdraw
 import numpy as np
+import math
 
 # main
 
+
+MAXDISPX = 500
+MAXDISPY = 400
+
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((500,400), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((MAXDISPX,MAXDISPY), 0, 32)
 
 pygame.display.set_caption('Animation')
 
@@ -18,13 +23,13 @@ fpsClock = pygame.time.Clock()
 #     events    #
 # ------------- #
 
-MOVELEFT = 97
+MOVELEFT  = 97
 MOVERIGHT = 100
-MOVEUP = 119
-MOVEDOWN = 115
+MOVEUP    = 119
+MOVEDOWN  = 115
 
 MOVESPEED = 1
-PROJVEL = 5
+PROJVEL   = 3
 
 # ------------- #
 #     colors    #
@@ -51,7 +56,9 @@ class Entity:
     def __init__(self, pos, dir, size, color, surf):
         self.pos = np.asarray(pos)
 
-        self.dir = dir
+        self.dir = float(dir)
+
+        self.cursorangle = 0
 
         self.size = size
 
@@ -59,37 +66,62 @@ class Entity:
 
         self.SURFACE = surf
 
-    def spawn(self):
-        pygame.gfxdraw.filled_circle(self.SURFACE, self.pos[0], self.pos[1], self.size, self.color)
+        self.speedMag = PROJVEL
 
+        self.speed = (float(self.speedMag*math.cos(self.dir)),
+                      float(self.speedMag*math.sin(self.dir)))
+
+    def spawn(self):
+        pygame.gfxdraw.filled_circle(self.SURFACE,
+                                     np.round(self.pos[0]).astype(int),
+                                     np.round(self.pos[1]).astype(int),
+                                     self.size,
+                                     self.color)
 
     def updatePos(self, dpos):
         pos = (self.pos + np.round(dpos)).astype(int)
         pygame.gfxdraw.filled_circle(self.SURFACE, pos[0], pos[1], self.size, self.color)
         self.pos = pos
 
-        pass
+    def updateProjectile(self):
+        self.pos = (self.pos + self.speed)
 
     def updateDir(self, dir):
         self.dir = dir
-        pass
 
+    def getCursorAngle(self):
+        mousepos = np.array(pygame.mouse.get_pos())
+        heading = mousepos - self.pos
+        print heading
+        return math.atan2(heading[1], heading[0])
+
+def removeCondition(entity):
+    if entity.pos[0] <= 0 or entity.pos[1] <= 0 or entity.pos[0] >= MAXDISPX or entity.pos[1] >= MAXDISPY:
+        return False
+    else:
+        return True
 
 # ------------- #
 #      main     #
 # ------------- #
 
-
-bob = Entity((200,200), (1, 1), 15, RED, DISPLAYSURF)
+bob = Entity((200,200), 1, 15, RED, DISPLAYSURF)
 
 projectiles = []
+
+
+hitboxes = []
+
 
 prevkey = 0
 hold = False
 
 while True:
     DISPLAYSURF.fill(WHITE)
+
+    # event handling
     for event in pygame.event.get():
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -115,6 +147,10 @@ while True:
         if event.type == KEYUP:
             hold = False
             prevkey = 0
+        if event.type == MOUSEBUTTONDOWN:
+            angle = bob.getCursorAngle()
+            print angle
+            projectiles.append(Entity((bob.pos[0], bob.pos[1]), angle, 5, BLACK, DISPLAYSURF))
 
     if hold:
         if prevkey == MOVELEFT or (hold & prevkey == MOVELEFT):
@@ -130,11 +166,18 @@ while True:
             print('left event hold')
             bob.updatePos(np.array([0, MOVESPEED]))
 
-    if not projectiles:
+    # projectile handling
+    if projectiles:
+        #print('Projectile(s) Present!')
+
         for proj in projectiles:
 
-            proj.spawn()
+            proj.updateProjectile()
 
+            proj.spawn()
+        # remove projectile from list if remove condition is met.
+        projectiles[:] = [x for x in projectiles if removeCondition(x)]
+    # entity handling
     bob.spawn()
 
     pygame.display.update()
